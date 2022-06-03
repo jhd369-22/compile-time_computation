42be2f18cad08ce91ac774ffd827e6ce15220322
Junhong Deng <dengjunhong@gmail.com>
2022-06-03 05:36:36 -0700

    update test

--- app/test_cexpr_basic_string.cpp
+++ app/test_cexpr_basic_string.cpp
@@ -5,10 +5,22 @@
 // catch2 template test cases
 TEMPLATE_TEST_CASE("empty_string constructor", "[cexpr_basic_string]", char, unsigned char, wchar_t) {
     constexpr ra::cexpr::cexpr_basic_string<TestType, 10> r;
-    CHECK(static_assert(r.size() == 0));
-    CHECK(static_assert(r.capacity() == 10));
-    CHECK(static_assert(r.max_size() == 10));
-    CHECK(static_assert(r.begin() == r.end()));
+    static_assert(r.size() == 0);
+    static_assert(r.capacity() == 10);
+    static_assert(r.max_size() == 10);
+    static_assert(r.begin() == r.end());
 }
 
+constexpr void testing_empty_string_constructor() {
+    constexpr ra::cexpr::cexpr_basic_string<char, 10> r;
+    static_assert(r.size() == 0);
+    static_assert(r.capacity() == 10);
+    static_assert(r.max_size() == 10);
+    static_assert(r.begin() == r.end());
+}
 
+int main(){
+    testing_empty_string_constructor();
+
+    return 0;
+}
