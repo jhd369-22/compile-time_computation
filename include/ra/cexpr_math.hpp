6004961f18e444d8bd6031dbc255905b7143d8b6
Junhong Deng <dengjunhong@gmail.com>
2022-06-07 04:37:17 -0700

    update

--- include/ra/cexpr_math.hpp
+++ include/ra/cexpr_math.hpp
@@ -52,14 +52,14 @@ namespace ra::cexpr_math {
     template <class T>
     constexpr T sin(T x) {
         if (x < 0) {
-            return -sin(-x);
+            return -sin<T>(-x);
         }
 
         if (x <= 10e-6) {
             return x;
         }
 
-        return 3.0 * sin(mod(x, 2.0 * pi<T>) / 3.0) - 4.0 * cube(sin(mod(x, 2.0 * pi<T>) / 3.0));
+        return 3.0 * sin<T>(mod<T>(x, 2.0 * pi<T>) / 3.0) - 4.0 * cube<T>(sin<T>(mod<T>(x, 2.0 * pi<T>) / 3.0));
     }
 
     // Returns the cosine of x.
@@ -68,7 +68,7 @@ namespace ra::cexpr_math {
     // The type T is a floating-point type.
     template <class T>
     constexpr T cos(T x){
-        return sin(x + pi<T> / 2.0);
+        return sin<T>(x + pi<T> / 2.0);
     }
 
     // Returns the tangent of x.
@@ -79,11 +79,11 @@ namespace ra::cexpr_math {
     // The type T is a floating-point type.
     template <class T>
     constexpr T tan(T x){
-        T value = cos(x);
+        T value = cos<T>(x);
         if(value == 0){
             throw std::overflow_error("tangent of infinite");
         }
-        return sin(x) / value;
+        return sin<T>(x) / value;
     }
 
     // Returns the square root of x.
@@ -110,14 +110,14 @@ namespace ra::cexpr_math {
         const T c = x;
         T n = 0.0;
 
-        while(abs(x_n - x_0) > epsilon && n < m){
+        while (abs<T>(x_n - x_0) > epsilon && n < m) {
             x_0 = x_n;
-            f_x = sqr(x_0) - c;
+            f_x = sqr<T>(x_0) - c;
             f_der = 2.0 * x_0;
             x_n = x_0 - f_x / f_der;
             n++;
         }
-        
+
         return x_n;
     }
 
